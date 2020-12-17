a79ace58df8e16e83bacb1e3cc825abe3e154efe
Henry Doan <henrydoan96@gmail.com>
2020-12-16 19:10:13 -0700

    back end done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # no longer or nav routes, api routes, 
+  # how we are reaching our controllers 
+  namespace :api do
+    resources :todos
+    # resources :comments
+  end
 end
